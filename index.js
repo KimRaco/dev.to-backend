270b1a4e55deb80d9776d6ad36b1e9314ce1d2aa
Kim Ramirez <kim.rc.1066@gmail.com>
2023-08-26 01:40:59 -0600

    feat: vercel added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import dbConnect from "./src/libs/db.js";
 import { server } from "./src/server.js";
+import * as dotenv from "dotenv";
 
+dotenv.config();
+
+const {SERVER_PORT} = process.env
+const port = SERVER_PORT || 8080
 
 dbConnect()
   .then(() => {
-    server.listen(8080, () => {
-      console.log(`Server listening on port 8080`);
+    server.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
     });
   })
   .catch(err => {
